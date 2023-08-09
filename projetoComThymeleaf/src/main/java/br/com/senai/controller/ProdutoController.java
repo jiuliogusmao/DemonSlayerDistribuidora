483df942ad40f33c3488199cfde0684c5f399802
murukluan <murukluan@gmail.com>
2023-08-09 19:04:05 -0300

    pagina de cadastro

--- projetoComThymeleaf/src/main/java/br/com/senai/controller/ProdutoController.java
+++ projetoComThymeleaf/src/main/java/br/com/senai/controller/ProdutoController.java
@@ -2,10 +2,15 @@ package br.com.senai.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import br.com.senai.model.Produto;
 import br.com.senai.repository.ProdutoRepository;
@@ -27,5 +32,24 @@ public class ProdutoController {
 		model.addAttribute("produtos", produtos);
 		return "produtos";
 	}
+	
+	@GetMapping("/cadastrarProduto")
+	public String paginaAdicionarProduto(Produto produto) {
+		return "adicionar_produto";
+	}
+	
+	@PostMapping("/adicionarProduto")
+	public String adicionaProduto(@Valid Produto produto, 
+			Errors erros, BindingResult result, Model model) {
+		if(result.hasErrors() || (null != erros && erros.getErrorCount() > 0)) {
+			return "adicionar_produto";
+		}
+		produtoRepository.save(produto);
+		return "redirect:/produto";
+	}
+	
+	
+	
+	
 
 }
