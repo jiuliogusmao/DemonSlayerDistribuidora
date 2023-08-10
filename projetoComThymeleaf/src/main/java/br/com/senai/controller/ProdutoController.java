20fe28f4fd8d6a832b6e2ce4e1b3f97a86e8b0f3
murukluan <murukluan@gmail.com>
2023-08-10 19:23:40 -0300

    criando editar

--- projetoComThymeleaf/src/main/java/br/com/senai/controller/ProdutoController.java
+++ projetoComThymeleaf/src/main/java/br/com/senai/controller/ProdutoController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import br.com.senai.model.Produto;
@@ -43,6 +44,29 @@ public class ProdutoController {
 			Errors erros, BindingResult result, Model model) {
 		if(result.hasErrors() || (null != erros && erros.getErrorCount() > 0)) {
 			return "adicionar_produto";
+		}
+		produtoRepository.save(produto);
+		return "redirect:/produto";
+	}
+	
+	@GetMapping("/editar/{id}")
+	public String paginaAtualizarProduto(
+			@PathVariable("id") long id, Model model) {
+		Produto produto = produtoRepository.findById(id)
+				.orElseThrow(() -> 
+				new IllegalArgumentException("Identificador do produto é inválido" + id));
+		
+		model.addAttribute("produto", produto);
+		return "editar_produto";
+	}
+	
+	@PostMapping("/atualizar/{id}")
+	public String atualizarProduto(@PathVariable("id") long id,
+			@Valid Produto produto, BindingResult result,
+			Model model) {
+		if(result.hasErrors()) {
+			produto.setId(id);
+			return "editar_produto";
 		}
 		produtoRepository.save(produto);
 		return "redirect:/produto";
--- projetoComThymeleaf/src/main/java/br/com/senai/model/Produto.java
+++ projetoComThymeleaf/src/main/java/br/com/senai/model/Produto.java
@@ -4,6 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Produto {
@@ -12,9 +16,16 @@ public class Produto {
 	@GeneratedValue( strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@NotNull(message = "Campo obrigatório")
+	@Size(min = 2, max = 254, message="Nome deve conter entre 2 e 254 caracteres")
 	private String nome;
+	
+	@Min(0)
 	private int quantidade;
+	
+	@Min(0)
 	private double preco;
+	
 	private String descricaoProduto;
 	private String urlImagem;
 	
